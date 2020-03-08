f55a77a492715fcd316a7ee0650c3d732bcba7c7
Josep Brugués <bruguesjosep@outlook.com>
2020-03-08 14:05:12 +0100

    Update mAP script

--- Week 2/Kitti/get_kitti_mAP.py
+++ Week 2/Kitti/get_kitti_mAP.py
@@ -7,25 +7,23 @@ def main(file):
     filename = os.path.join(file)
     if os.path.exists(filename):
         with open(filename, 'r') as f:
-            vLines = f.readlines()
-            vLines = np.asarray([[float(number) for number in line.rstrip().split(' ')] for line in vLines])
-        mean_scores = np.mean(vLines, axis=1)
-        return mean_scores
+            lines = f.readlines()
+        
+        easy_score = np.mean([float(number) for number in lines[0].rstrip().split(' ')])
+        moderate_score = np.mean([float(number) for number in lines[1].rstrip().split(' ')])
+        hard_score = np.mean([float(number) for number in lines[2].rstrip().split(' ')])
+        return (easy_score + moderate_score + hard_score) / 3
     return np.zeros(3, dtype=np.float32)
 
 
 if __name__ == '__main__':
 
-    files = glob.glob('./cpp/output_all_classes_resnext101/plot/*.txt')
+    files = glob.glob('./cpp/output_7_classes_resnext101/*.txt')
     files.sort()
     maps = {}
 
     for file in files:
-        values = main(file)
-        sum = 0.0
-        for i in range(41):
-            sum += values[i]
-        maps[file] = sum/41
+        maps[file] = main(file)
 
     for key, value in maps.items():
         print(key, ' ', value)
