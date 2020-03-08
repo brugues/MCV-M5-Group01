05605dbb7c5cef8cefd4751e47208bad79916435
sergiocasaspastor <scasap00@gmail.com>
2020-03-08 14:44:30 +0100

    retinanet and faster-rcnn inference

---
+++ Week 2/RetinaNet/detectron2_retinanet.py
@@ -0,0 +1,58 @@
+import detectron2
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
+# import some common libraries
+import numpy as np
+import cv2
+import random
+import glob
+# from google.colab.patches import cv2_imshow
+
+# import some common detectron2 utilities
+from detectron2 import model_zoo
+from detectron2.engine import DefaultPredictor
+from detectron2.config import get_cfg
+from detectron2.utils.visualizer import Visualizer
+from detectron2.data import MetadataCatalog
+
+# get image
+TRAIN_PATH = 'MIT_split/train/'
+TEST_PATH = 'MIT_split/test/'
+# TRAIN_PATH = '../mcv/datasets/MIT_split/train/'
+# TEST_PATH = '../mcv/datasets/MIT_split/test/'
+
+# Create config
+cfg = get_cfg()
+
+cfg.merge_from_file(model_zoo.get_config_file("COCO-Detection/retinanet_R_101_FPN_3x.yaml"))
+cfg.MODEL.WEIGHTS = model_zoo.get_checkpoint_url("COCO-Detection/retinanet_R_101_FPN_3x.yaml")
+cfg.MODEL.RETINANET.SCORE_THRESH_TEST = 0.5 # set threshold for this model
+
+# Create predictor
+predictor = DefaultPredictor(cfg)
+
+# Read images
+folders = []
+nimg = 0
+
+for item in glob.glob(TEST_PATH + '/*'):
+    folders.append(item.split('/')[-1])
+    
+# for folder in folders:
+images = glob.glob(TEST_PATH + "street" + '/*') 
+for image in images: 
+    nimg = nimg + 1
+    print("Image: ", nimg)
+    im = cv2.imread(image)
+    im = cv2.resize(im,(1024,1024))
+
+    # Make prediction
+    outputs = predictor(im)
+    if nimg == 10:
+        print(outputs["instances"].to("cpu"))
+
+    # Visualize result
+    v = Visualizer(im[:, :, ::-1], MetadataCatalog.get(cfg.DATASETS.TRAIN[0]), scale=1.2)
+    v = v.draw_instance_predictions(outputs["instances"].to("cpu"))
+    cv2.imwrite("object_detection/" + image, v.get_image()[:, :, ::-1])
